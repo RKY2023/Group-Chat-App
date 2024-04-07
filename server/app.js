1cbc155c462eff2a6c2dc9cc5f0564d82f897974
RKY <xraj2023@gmail.com>
2024-04-07 19:46:33 +0530

    Creating the ChatApp Backend

--- server/app.js
+++ server/app.js
@@ -6,11 +6,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const sequelize = require("./util/database");
-// const User = require("./models/user");
+const User = require("./models/user");
+const Thread = require("./models/thread");
 
 const app = express();
 
 const userRoutes = require("./routes/user");
+const threadRoutes = require("./routes/thread");
 
 app.use(cors({
     origin: ["http://localhost:3000", "http://127.0.0.1:5000"]
@@ -19,6 +21,10 @@ app.use(cors({
 app.use(bodyParser.json());
 
 app.use(userRoutes);
+app.use(threadRoutes);
+
+User.hasMany(Thread);
+Thread.belongsTo(User);
 
 sequelize
   // .sync({ force: true})
