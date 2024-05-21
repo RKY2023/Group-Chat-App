31bc87c910cd865a69bb4601419648bbf515d7b6
RKY <xraj2023@gmail.com>
2024-05-21 11:35:02 +0530

    deploy

---
+++ client/src/components/group/groupInfo/GroupMembers.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import CardList from '../../Common/CardList';
+import { grf1 } from "../../../assets/groupchat";
+
+function GroupMembers() {
+  const groupMembers = useSelector(state => state.group.groupMembers);
+  return (
+    <>
+    {groupMembers.map((group, i) => {
+      return (
+        <CardList
+          elem-type={'groupMembers'}
+          id={group.id}
+          userid={group.userid}
+          gp={grf1}
+          name={group.name}
+          isAdmin={group.isAdmin}
+        />
+      );
+    })}
+    </>
+  )
+}
+
+export default GroupMembers
---
+++ client/src/components/group/groupInfo/GroupProfile.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import RoundedBtn from "../../Common/RoundedBtn";
+import { BiEditAlt } from "react-icons/bi";
+import { useSelector } from "react-redux";
+import { FaUserGroup } from "react-icons/fa6";
+
+function GroupProfile() {
+  const groupMembers = useSelector(state => state.group.groupMembers);
+  return (
+    <div className="flex flex-col justify-between items-center bg-{#202d33] h-[300px] p-3 text-white">
+      <div className="flex justify-center items-center rounded-full p-5 bg-[#45454430]">
+      {/* <img src={pp} alt="profile_picture" className="rounded-full w-[40px]" /> */}
+        
+      <RoundedBtn icon={<FaUserGroup />}/>
+      </div>
+      <div className="flex justify-between items-center">
+        <span>
+          Group Name
+        </span>
+        <RoundedBtn icon={<BiEditAlt />} />
+      </div>
+      <div className="flex justify-between items-center">
+        <span>
+          Group | {groupMembers.length} members
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default GroupProfile
