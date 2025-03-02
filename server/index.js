fd215c9d7756e56a226ce6812a8d3545705ba76f
raj <xraj2023@gmail.com>
2025-03-02 15:26:20 +0530

    debugging

--- server/index.js
+++ server/index.js
@@ -7,18 +7,18 @@ console.log('PORT', PORT);
 
 const bodyParser = require("body-parser");
 
-// utils
-const connectDB = require("./util/database");
+// // utils
+// const connectDB = require("./util/database");
 
-// routes
-const userRoutes = require("./routes/user");
-const threadRoutes = require("./routes/thread");
+// // routes
+// const userRoutes = require("./routes/user");
+// const threadRoutes = require("./routes/thread");
 
 const app = express();
 
-app.use(cors({
-    origin: [process.env.CLIENT_URL_DEV, process.env.CLIENT_URL],
-}));
+// app.use(cors({
+//     origin: [process.env.CLIENT_URL_DEV, process.env.CLIENT_URL],
+// }));
 
 console.log('process.env.CLIENT_URL_DEV', process.env.CLIENT_URL_DEV);
 console.log('process.env.CLIENT_URL', process.env.CLIENT_URL);
@@ -28,11 +28,10 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 console.log('root working', process.env.MONGO_URI);
-app.use(userRoutes);
-app.use(threadRoutes);
-git 
-connectDB().then(() => {
+// app.use(userRoutes);
+// app.use(threadRoutes);
+// connectDB().then(() => {
     app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
-}).catch(err => console.log(err));
+// }).catch(err => console.log(err));
 
 module.exports = app;
