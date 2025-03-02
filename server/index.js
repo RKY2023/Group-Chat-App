85ff4ce7a91cc1b97bf266c2cd6ad79974d0b097
raj <xraj2023@gmail.com>
2025-03-02 15:30:40 +0530

    debugging

--- server/index.js
+++ server/index.js
@@ -3,35 +3,30 @@ const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 
-console.log('PORT', PORT);
-
 const bodyParser = require("body-parser");
 
-// // utils
-// const connectDB = require("./util/database");
+// utils
+const connectDB = require("./util/database");
 
-// // routes
-// const userRoutes = require("./routes/user");
-// const threadRoutes = require("./routes/thread");
+// routes
+const userRoutes = require("./routes/user");
+const threadRoutes = require("./routes/thread");
 
 const app = express();
 
-// app.use(cors({
-//     origin: [process.env.CLIENT_URL_DEV, process.env.CLIENT_URL],
-// }));
-
-console.log('process.env.CLIENT_URL_DEV', process.env.CLIENT_URL_DEV);
-console.log('process.env.CLIENT_URL', process.env.CLIENT_URL);
+app.use(cors({
+    origin: [process.env.CLIENT_URL_DEV, process.env.CLIENT_URL],
+}));
 
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-console.log('root working', process.env.MONGO_URI);
-// app.use(userRoutes);
-// app.use(threadRoutes);
-// connectDB().then(() => {
+app.use(userRoutes);
+app.use(threadRoutes);
+
+connectDB().then(() => {
     app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
-// }).catch(err => console.log(err));
+}).catch(err => console.log(err));
 
 module.exports = app;
