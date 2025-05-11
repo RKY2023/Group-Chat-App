b2270100f7044806b9278d9f691fa32f166af1a8
raj <xraj2023@gmail.com>
2025-05-11 18:44:20 +0530

    check

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ const app = express();
 
 app.use(cors({
     origin: [process.env.CLIENT_URL_DEV, process.env.CLIENT_URL],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
 app.use(bodyParser.json());
